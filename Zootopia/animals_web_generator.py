5b5cbf0fbd5f17d41ed5a8fb5afe71471c3c5d77
Maik Scherder <maikscherder1993@gmail.com>
2025-01-25 18:43:12 +0100

    Removed Abundant code and improved card item html

--- Zootopia/animals_web_generator.py
+++ Zootopia/animals_web_generator.py
@@ -6,39 +6,18 @@ def load_data(file_path):
 
 animals_data = load_data("animals_data.json")
 
-def print_animal_info(animal):
-    """Prints animal information
-    Args:
-      animal: A dictionary showing an animal from the JSON data.
-    """
-    if 'name' in animal:
-        print(f"Name: {animal['name']}")
-    if 'characteristics' in animal and 'diet' in animal['characteristics']:
-        print(f"Diet: {animal['characteristics']['diet']}")
-    if 'locations' and animal['locations']:
-        print(f"Location: {animal['locations'][0]}")
-    if 'characteristics' in animal and 'type' in animal['characteristics']:
-        print(f"Type: {animal['characteristics']['type']}")
-    print()
-
-
-with open('animals_data.json', 'r') as f:
-    data = json.load(f)
-    for animal in data:
-        print_animal_info(animal)
-
 def generate_animal_html(animal):
     """Generates HTML string for a single animal."""
-    html_str = "<li class='cards__item'>"
-    if 'name' in animal:
-        html_str += f"Name: {animal['name']}<br>"
-    if 'characteristics' in animal and 'diet' in animal['characteristics']:
-        html_str += f"Diet: {animal['characteristics']['diet']}<br>"
-    if 'locations' and animal['locations']:
-        html_str += f"Location: {animal['locations'][0]}<br>"
-    if 'characteristics' in animal and 'type' in animal['characteristics']:
-        html_str += f"Type: {animal['characteristics']['type']}<br>"
-    html_str += "</li>"
+    html_str = f"""
+    <li class="cards__item">
+        <div class="card__title">{animal['name']}</div>
+        <p class="card__text">
+            <strong>Diet:</strong> {animal['characteristics'].get('diet')}<br/> 
+            <strong>Location:</strong> {', '.join(animal['locations'])}<br/> 
+            <strong>Type:</strong> {animal['characteristics'].get('type')}<br/>
+        </p>
+    </li>
+    """
     return html_str
 
 def generate_html_content(animals):
@@ -46,7 +25,7 @@ def generate_html_content(animals):
     html_list = ""
     for animal in animals:
         html_list += generate_animal_html(animal)
-    return f"<ul class='cards'>{html_list}</ul>"
+    return html_list
 
 def main():
     """Reads template, generates HTML, and writes to a new file."""
